4664a29e5ac74d88827e4262f9df56a4c43384dd
liuyang-1990 <xy_liuy0305@163.com>
2020-03-19 22:18:34 +0800

    fix doc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,15 +8,22 @@ export default class AppDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <meta name="keywords" content="刘洋的个人网站，刘洋的博客，web开发，nodejs全栈，前端工程师，后端开发，docker容器，生活日常"/>
-                    <meta name="description" content="刘洋的个人网站，专注于web开发，尤其是前端开发。喜欢做技术，也喜欢分享技术。本站主要是分享web相关文章内容，以及个人工作相关日志！"/>
+                    <meta charSet="utf-8" />
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+                    <meta httpEquiv="Window-target" content="_top" />
+                    <meta name="renderer" content="webkit" />
+                    <meta name="keywords" content="刘洋的个人网站，刘洋的博客，web开发，nodejs全栈，前端工程师，后端开发，docker容器，生活日常" />
+                    <meta name="description" content="刘洋的个人网站，专注于web开发，尤其是前端开发。喜欢做技术，也喜欢分享技术。本站主要是分享web相关文章内容，以及个人工作相关日志！" />
                     <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+    
                 </Head>
-                <body>
+                <body className="user-select">
                     <Main />
                     <NextScript />
                     <script dangerouslySetInnerHTML={{
-                        __html:`                      
+                        __html: `                      
                         var _mtac = {"performanceMonitor":1,"senseQuery":1};
                         (function() {
                             var mta = document.createElement("script");
