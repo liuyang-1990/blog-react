860372fae638f9231a1f355b63b7c5964df41bb7
ll5u <luis.y.liu@newegg.com>
2020-03-22 14:56:29 +0800

    fix some config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,47 +1,33 @@
 import App from 'next/app';
 import React from 'react';
 import { Spin } from 'antd';
-import { fetchInitialStoreState, Store } from '../../store';
-import { Provider } from 'mobx-react';
 import { ProgressLoading, MasterPage } from '../components';
 import { LoadingOutlined } from '@ant-design/icons';
 import '../styles/global.less';
 
-
 Spin.setDefaultIndicator(<LoadingOutlined spin style={{ fontSize: '180%', marginTop: 30 }} />);
 
-class MyMobxApp extends App {
-  state = {
-    store: new Store(),
-  }
+class CustomApp extends App {
 
   // Fetching serialized(JSON) store state
   static async getInitialProps(appContext) {
-    const appProps = await App.getInitialProps(appContext)
-    const initialStoreState = await fetchInitialStoreState()
+    const appProps = await App.getInitialProps(appContext);
 
     return {
-      ...appProps,
-      initialStoreState,
+      ...appProps
     }
-  }
-
-  // Hydrate serialized state to store
-  static getDerivedStateFromProps(props, state) {
-    state.store.hydrate(props.initialStoreState)
-    return state
   }
 
   render() {
     const { Component, pageProps } = this.props
     return (
-      <Provider store={this.state.store}>
+      <>
         <ProgressLoading showAfterMs={100} />
         <MasterPage {...pageProps}>
           <Component {...pageProps} />
         </MasterPage>
-      </Provider>
+      </>
     )
   }
 }
-export default MyMobxApp
+export default CustomApp
